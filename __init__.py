4b58f2f7f9baca22e9c35617d6319c2f8aeb0dd0
Mctmarcus3 <Mctmarcus3@gmail.com>
2020-12-31 12:14:38 +0800

    Created master template, Please fetch frequently.

--- __init__.py
+++ __init__.py
@@ -2,25 +2,19 @@ from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/local')
-def home():
-    return "<h1>Local File</>"
-
-@app.route('/Test')
-def home():
-    return "<h1>Local File</>"
 
 @app.route('/login')
 def login():
     return render_template('index.html')
 
+
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('index.html')
+    return render_template('404.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
