686cb45dacf08decfb66401e6bcbb4a995b7a9bb
Mctmarcus3 <Mctmarcus3@gmail.com>
2021-01-04 22:54:20 +0800

    added some new code accounts, inventory and sliders(Not Working)

--- __init__.py
+++ __init__.py
@@ -7,14 +7,21 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
 @app.route('/login')
 def login():
     return render_template('index.html')
 
 
+@app.route('/inventory')
+def inventory():
+    return render_template('inventory.html')
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
