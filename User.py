3236dc4e05471539de3cf2e5c09c91f5ffd26c1d
Mctmarcus3 <Mctmarcus3@gmail.com>
2021-01-16 21:11:47 +0800

    Added other changes

---
+++ User.py
@@ -0,0 +1,16 @@
+class User:
+    def __init__(self, email, password):
+        self.__email = email
+        self.__password = password
+
+    def set_email(self, email):
+        self.__email = email
+
+    def get_email(self):
+        return self.__email
+
+    def set_password(self, password):
+        self.__password = password
+
+    def get_password(self):
+        return self.__password
--- inventoryForm.py
+++ inventoryForm.py
@@ -10,3 +10,12 @@ class CreateInventoryForm(Form):
                       validators.DataRequired()])  # TODO: Implement Bar Code Reader
     price = DecimalField('Price', [validators.NumberRange(min=0),
                          validators.DataRequired()], places=2, rounding=None)
+
+
+class UpdateInventoryForm(Form):
+        productName = StringField('ProductName', [validators.Length(min=1, max=80),
+                                  validators.DataRequired()])
+        stock = IntegerField('Stock',  [validators.NumberRange(min=0),
+                             validators.DataRequired()])
+        price = DecimalField('Price', [validators.NumberRange(min=0),
+                             validators.DataRequired()], places=2, rounding=None)
