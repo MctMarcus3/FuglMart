cbb6a4e505c1d4aa7ea71b812f7854662671ffcb
Zhiquan03 <zhiquan03w@gmail.com>
2021-02-01 15:01:40 +0800

    CR(U)(D)

--- shoppingcart/addshoppingcart.py
+++ shoppingcart/addshoppingcart.py
@@ -7,5 +7,5 @@ class AddshoppingcartForm(Form):
     item_code = StringField('Item code', [validators.Optional()])
     item_name = StringField('Item name', [validators.Optional()])
     item_amount = StringField('Item amount', [validators.Optional()])
-    receit_number = StringField('Receipt number', [validators.Optional()])
+    receipt_number = StringField('Receipt number', [validators.Optional()])
     item_price = TextAreaField('Item price', [validators.Optional()])
--- shoppingcart/shoppingcart.py
+++ shoppingcart/shoppingcart.py
@@ -1,11 +1,11 @@
 class Item:
     item_count = 0
-    def __init__(self, item_name, item_amount, item_price, item_code, item_count=0):
+    def __init__(self, item_name, item_amount, item_price, item_code, item_count=0,):
         Item.item_count += 1
         self.__item_no = Item.item_count
         self.__item_name = item_name
         self.__item_amount = item_amount
-        self.__receit_number = item_count
+        self.__receipt_number = item_count
         self.__item_price = item_price
         self.__item_code = item_code
 
@@ -18,8 +18,8 @@ class Item:
     def get_item_amount(self):
         return self.__item_amount
 
-    def get_receit_number(self):
-        return self.__receit_number
+    def get_receipt_number(self):
+        return self.__receipt_number
 
     def get_item_price(self):
         return self.__item_price
@@ -33,8 +33,8 @@ class Item:
     def set_item_amount(self, item_amount):
         self.__item_amount = item_amount
 
-    def set_receit_number(self, receit_number):
-        self.__receit_number = receit_number
+    def set_receipt_number(self, receipt_number):
+        self.__receipt_number = receipt_number
 
     def set_item_price(self, item_price):
         self.__item_price = item_price
