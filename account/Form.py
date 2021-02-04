5387d3369f098c9b29a2ffc78767f19326bba828
Zhiquan03 <zhiquan03w@gmail.com>
2021-02-04 11:01:44 +0800

    Revert "Merge branch 'main' of https://github.com/MctMarcus3/FuglMart into main"
    This reverts commit 7fb6cdd07423072cfdf384743ba3858bc61f7cc6, reversing
    changes made to cbb6a4e505c1d4aa7ea71b812f7854662671ffcb.

--- account/Form.py
+++ account/Form.py
@@ -1,22 +1,13 @@
 from wtforms import Form, validators, PasswordField
 from wtforms.fields.html5 import EmailField
-from wtforms import IntegerField, StringField
 import email_validator
 
 
 class CreateUserForm(Form):
     email = EmailField('Email', [validators.Email(), validators.DataRequired()], render_kw={"placeholder": "cookie@fugl.store"})
-    password = PasswordField('Password', [validators.Length(min=1, max=150), validators.DataRequired()])
-    username = StringField(label=('Username'))
-    number = IntegerField('Mobile Number', [validators.NumberRange(min=80000000, max=99999999), validators.DataRequired()])
-    postal_code = IntegerField('Postal Code', [validators.NumberRange(min=100000, max=999999), validators.DataRequired()])
-
-class LoginUserForm(Form):
-    email = EmailField('Email', [validators.Email(), validators.DataRequired()], render_kw={"placeholder": "cookie@fugl.store"})
-    password = PasswordField('Password', [validators.Length(min=1, max=150), validators.DataRequired()])
+    password = PasswordField('Password',
+                             [validators.Length(min=1, max=150),
+                              validators.DataRequired()])
 
 class UserProfile(Form):
     email = EmailField('Email', [validators.Email(), validators.DataRequired()], render_kw={"placeholder": "cookie@fugl.store"})
-    username = StringField('Username')
-    number = IntegerField('Mobile Number', [validators.NumberRange(min=80000000, max=99999999), validators.DataRequired()])
-    postal_code = IntegerField('Postal Code', [validators.NumberRange(min=100000, max=999999), validators.DataRequired()])
